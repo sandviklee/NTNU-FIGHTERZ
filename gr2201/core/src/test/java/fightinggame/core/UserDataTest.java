4ceb740e5f21e8c0aaaa6747d64bef7695cf2796
Sverre Nystad <sverrnys@ntnu.no>
2022-09-19 01:29:38 +0200

    Made test for constructor of UserData and UserId

--- gr2201/core/src/test/java/fightinggame/core/UserDataTest.java
+++ gr2201/core/src/test/java/fightinggame/core/UserDataTest.java
@@ -1,5 +1,40 @@
 package fightinggame.core;
 
+import static org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Test;
+
+
 public class UserDataTest {
 	
+
+	private String validePassword = "Password1";
+	private String toShortPassword = "";
+	private String specialCharactersPassword = "p.,sad";
+
+	private UserData data;
+	
+	@Test
+	@DisplayName
+	public void testConstructor() {
+		// Contructed with bad input values
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new UserData(toShortPassword);
+		}, "This UserData is not possible");
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new UserData(specialCharactersPassword);
+		}, "This UserData is not possible");
+
+		data = new UserData(validePassword);
+		assertTrue(data.getPassword().equals(validePassword), "Does not have correct field");
+	}
+
+	@Test
+	@DisplayName
+	public void test() {
+
+	}
+
 }
--- gr2201/core/src/test/java/fightinggame/core/UserIdTest.java
+++ gr2201/core/src/test/java/fightinggame/core/UserIdTest.java
@@ -1,5 +1,43 @@
 package fightinggame.core;
+import static org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Test;
 
 public class UserIdTest {
+	private String valideId = "Username1";
+	private String otherValideId = "Username2";
+
+	private String toShortId = "";
+	private String specialCharactersId = ".";
+
+	private UserId id;
+	private UserId otherId;
+
 	
+	@Test
+	@DisplayName
+	public void testConstructor() {
+		// Contructed with bad input values
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new UserId(toShortId);
+		}, "This UserId is not possible");
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			new UserId(specialCharactersId);
+		}, "This UserId is not possible");
+
+		id = new UserId(validePassword);
+		assertTrue(id.getUserId().equals(validePassword), "Does not have correct field");
+	}
+
+	@Test
+	@DisplayName
+	public void testEquals() {
+		id = new UserId(valideId);
+		otherId = new UserId(otherValideId);
+		assertFalse(id.equals(otherId), "userIds is not the same");
+		assertTrue(id.equals(id), "userId should be the same");
+	}
+
 }
