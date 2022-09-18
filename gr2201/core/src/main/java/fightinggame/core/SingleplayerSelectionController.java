970b68a699ed44c92bd998e605aa02f5ccc7c866
Elin <elihaugu@stud.ntnu.no>
2022-09-18 19:56:53 +0200

    Added SingleplayerSelectionController.java | Issue #7

---
+++ gr2201/core/src/main/java/fightinggame/core/SingleplayerSelectionController.java
@@ -0,0 +1,45 @@
+package fightinggame.core;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.GridPane;
+
+public class SingleplayerSelectionController {
+    @FXML private Button lockIn;
+    @FXML private GridPane characterSelectGrid;
+    @FXML private ImageView characterSelected;
+
+    @FXML
+    private void initialize(){
+        lockIn.setDisable(true);
+    }
+
+    @FXML
+    private void handleSelectCharacter(ActionEvent event) {
+        ImageView image = (ImageView) event.getSource();
+        characterSelected.setImage(new Imageview(image));
+        image.setOpacity(0.7);
+        lockIn.setDisable(false);
+    }
+
+    @FXML
+    private void handleLockIn(ActionEvent event) throws IOException {
+        // TODO: Future content
+        // FXMLLoader loader = new FXMLLoader(getClass().getResource("singleplayer.fxml"));
+        // Parent root = loader.load();
+        // SignUpController signUpController = loader.getController();
+        // SceneController.changeScene("NTNU Fighterz", root, event);
+
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setContentText("This part of the game is not implemented yet :)");
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void handleGoBack(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainMenu.fxml"));
+        Parent root = loader.load();
+        SignUpController signUpController = loader.getController();
+        SceneController.changeScene("NTNU Fighterz", root, event);
+    }
+}
