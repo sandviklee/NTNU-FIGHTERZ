4a0f077fd8de8060d7c6c114142a323bb1439c56
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-09-22 11:42:30 +0200

    Make GameApp

--- gr2201/core/src/main/java/fightinggame/core/GameApp.java
+++ gr2201/core/src/main/java/fightinggame/core/GameApp.java
@@ -1,5 +1,13 @@
 package fightinggame.core;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+
 public class GameApp extends Application  {
 	public static void main(String[] args) {
 		Application.launch(args);
