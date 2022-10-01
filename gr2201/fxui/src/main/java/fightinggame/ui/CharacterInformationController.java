ca20c4a6e774c80f4d31e9ff6a088899834f0031
SaiYue <74411800+SaiYueWasTaken@users.noreply.github.com>
2022-10-01 15:57:12 +0200

    removed Imports in Controller

--- gr2201/fxui/src/main/java/fightinggame/ui/CharacterInformationController.java
+++ gr2201/fxui/src/main/java/fightinggame/ui/CharacterInformationController.java
@@ -1,13 +1,10 @@
 package fightinggame.ui;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
