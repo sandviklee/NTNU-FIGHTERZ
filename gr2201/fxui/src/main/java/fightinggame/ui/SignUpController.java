48ef029abbf2ecc59b3e6bb9558bb6578495f234
Sverre Nystad <sverrnys@ntnu.no>
2022-10-03 01:28:51 +0200

    Make SignUpController change name to SignupController Remove unused imports

--- gr2201/fxui/src/main/java/fightinggame/ui/SignUpController.java
+++ gr2201/fxui/src/main/java/fightinggame/ui/SignUpController.java
@@ -3,19 +3,14 @@ import fightinggame.users.LoginSignup;
 import fightinggame.users.User;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import javafx.scene.Node;
 
 
 public class SignupController extends SceneController{
