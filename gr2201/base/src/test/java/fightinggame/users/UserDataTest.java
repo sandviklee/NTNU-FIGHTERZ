cb12f0698e3ac63f588fdd85acfa6a8f4276ae69
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-10-11 19:51:39 +0000

    Make UserDataTest more

--- gr2201/base/src/test/java/fightinggame/users/UserDataTest.java
+++ gr2201/base/src/test/java/fightinggame/users/UserDataTest.java
@@ -15,11 +15,13 @@ public class UserDataTest {
 	
 
 	private String validePassword = "Password1";
+	private String validePasswordOther = "Password2";
 	private String toShortPassword = "";
 	private String specialCharactersPassword = "p.,sad";
 
-	private UserData data;
-	
+	private UserData data1;
+	private UserData data2;
+
 	@Test
 	@DisplayName("Tests if the constructor works properly")
 	public void testConstructor() {
@@ -32,13 +34,17 @@ public class UserDataTest {
 			new UserData(specialCharactersPassword);
 		}, "This UserData is not possible");
 
-		data = new UserData(validePassword);
-		assertTrue(data.getPassword().equals(validePassword), "Does not have correct field");
+		data1 = new UserData(validePassword);
+		assertTrue(data1.getPassword().equals(validePassword), "Does not have correct field");
 	}
 
 	@Test
-	@DisplayName("Tests if the ")
-	public void testStringToData() {
-		//TODO: make a string an try to get correct UserDAta
+	@DisplayName("Tests if the equals works properly")
+	public void TestEquals() {
+		UserData data1 = new UserData(validePassword);
+		UserData data2 = new UserData(validePasswordOther);
+
+		assertTrue(data1.equals(data1));
+		assertFalse(data1.equals(data2));
 	}
 }
