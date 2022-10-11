805533aae5136862c0a89b283a44ad7d42f100ff
Elin <elihaugu@stud.ntnu.no>
2022-10-11 15:22:51 +0200

    Add dependency between fxui and gameplay module

--- gr2201/fxui/src/main/java/module-info.java
+++ gr2201/fxui/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module fightinggame.ui {
     requires javafx.fxml;
     requires transitive javafx.graphics;
     requires fightinggame.base;
+    requires fightinggame.gameplay;
     
     opens fightinggame.ui to javafx.fxml, javafx.graphics;
     exports fightinggame.ui;
