115906acd921fc9987f9b62827a65b8e7325616d
Kristoffer Nohr Olaisen <krisnol@stud.ntnu.no>
2022-10-12 21:54:27 +0000

    add test for button back to main menu

--- gr2201/fxui/src/test/java/fightinggame/ui/SingleplayerSelectionControllerTest.java
+++ gr2201/fxui/src/test/java/fightinggame/ui/SingleplayerSelectionControllerTest.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class SingleplayerSelectionControllerTest extends ApplicationTest{
     private SingleplayerSelectionController controller;
@@ -27,11 +28,29 @@ public class SingleplayerSelectionControllerTest extends ApplicationTest{
     @Test
     public void testSelectCharacter(){
         clickOn("#Ram");
-        Assertions.assertEquals("javafx.scene.image.Image@f0c8a99", controller.getCharacterSelected().getImage().toString());
+        Assertions.assertEquals("javafx.scene.image.Image@1cf6d1be", controller.getCharacterSelected().getImage().toString());
         clickOn("#Sly");
-        Assertions.assertEquals("javafx.scene.image.Image@76a4ebf2", controller.getCharacterSelected().getImage().toString());
+        Assertions.assertEquals("javafx.scene.image.Image@4b013c76", controller.getCharacterSelected().getImage().toString());
         clickOn("#Sol");
-        Assertions.assertEquals("javafx.scene.image.Image@53fe15ff", controller.getCharacterSelected().getImage().toString());
+        Assertions.assertEquals("javafx.scene.image.Image@cb0755b", controller.getCharacterSelected().getImage().toString());
+    }
+
+    @Test
+    public void testBack(){
+        clickOn("#goBack");
+        Assertions.assertNotNull(getCurrentRootById("mainMenuRoot"), "Wrong root when pressing goBack button");
+        
+    }
+
+    private Parent getCurrentRootById(String id){
+        for (Window window: Window.getWindows()){
+            if (window.isShowing() && window instanceof Stage){
+                if (window.getScene().getRoot().getId().equals(id)){
+                    return window.getScene().getRoot();
+                }
+            }
+        }
+        return null;
     }
 
    
