c33fc9a23630e0499e86946f9122b0596db211f1
Elin <elihaugu@stud.ntnu.no>
2022-10-13 23:07:37 +0200

    Add first iteration gameplay World

--- gr2201/gameplay/src/main/java/fightinggame/game/World.java
+++ gr2201/gameplay/src/main/java/fightinggame/game/World.java
@@ -1,4 +1,55 @@
 package fightinggame.game;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
 public class World {
-    
+    private ArrayList<WorldEntity> worldEntities;
+    private GameCharacter gameCharacter1;
+    private GameCharacter gameCharacter2;
+
+    public World(ArrayList<WorldEntity> worldEntities){
+        this.worldEntities = worldEntities;
+        for (WorldEntity entity : worldEntities) {
+            if(entity.getClass().equals(GameCharacter.class)){
+                if(gameCharacter1==null){
+                    gameCharacter1 = (GameCharacter) entity;
+                } else {
+                    gameCharacter2 = (GameCharacter) entity;
+                }
+            }
+        }
+    }
+
+    public void addWorldEntity(WorldEntity worldEntity){
+        this.worldEntities.add(worldEntity);
+    }
+
+    public void updateWorld(ArrayList<String> input){
+        handleCollisions();
+        setActions(input);
+        applyActions();
+    }
+
+    private void handleCollisions(){
+        for (WorldEntity entity1 : worldEntities) {
+            for (WorldEntity entity2 : worldEntities) {
+                if(entity1.contains(entity2)){
+                    System.out.println("Collision between" + entity1 + "and" + entity2);
+                }
+            }
+        }
+    }
+
+    private void setActions(ArrayList<String> input){
+        String inputString = String.join("", input);
+        String validInput = inputString.replaceAll("[^WASDKL ]", "");
+        gameCharacter.setCurrentAction(validInput);
+    }
+
+    private void applyActions(){
+        for (WorldEntity worldEntity : worldEntities) {
+            worldEntity.doAction();
+        }
+    }
 }
