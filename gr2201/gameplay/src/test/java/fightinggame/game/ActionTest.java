c7c626f96637fdcea08b4b29260288e16c76faa5
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-10-13 13:50:55 +0000

    Make actionTest constructor test

--- gr2201/gameplay/src/test/java/fightinggame/game/ActionTest.java
+++ gr2201/gameplay/src/test/java/fightinggame/game/ActionTest.java
@@ -9,16 +9,44 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ActionTest {
-   
+    private String validSpriteName;
+    private int validDuration;
+    private int validActionPriority1;
+    private int validActionPriority2;
+    private int validActionPriority3;
+
 
     private int negNum;
+    private Boolean truthy;
+    private Boolean falsy;
+    private Effectbox box;
+    private Vector vec;
+
+
+    private Action actionCantBeInterupted;
+    private Action actionCanBeSelfInterupted;
+    private Action actionCanBeEnemyInterupted;
+
 
     @BeforeEach
     public void setup() {
-
+        
     } 
 
     @Test
     @DisplayName("Check if constructor creates correct instances")
-    public void 
+    public void testConstructor() {
+
+        // First constructor: with EffectBox
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Action(box, validSpriteName, truthy, 
+                truthy, vec, validDuration, actionPriority, 
+                hitBoxStartTime, damage, totalFrames, animationLoop, animationLoopStartTime);
+		}, "This AnimationSprite is not possible");
+
+
+        // Second constructor:
+
+    }
 }
