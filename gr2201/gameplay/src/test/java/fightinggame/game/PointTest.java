101d7fb88244b5f875e649234b45a1ad79926cd2
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-10-20 02:41:55 +0000

    Make PointTest and VectorTest

---
+++ gr2201/gameplay/src/test/java/fightinggame/game/PointTest.java
@@ -0,0 +1,36 @@
+package fightinggame.game;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PointTest {
+    
+
+    @Test
+    @DisplayName("Test if the constructor creates correct object with correct fields")
+    public void testConstructor() {
+
+    }
+
+    @Test
+    @DisplayName("Test if moves point correctly")
+    public void testMove() {
+
+    }
+
+    @Test
+    public void testAddActionVector() {
+        
+    }
+
+    @Test
+    public void testSetActionVector() {
+        
+    }
+
+    @Test
+    void testSetGravityVector() {
+        
+    }
+
+}
---
+++ gr2201/gameplay/src/test/java/fightinggame/game/VectorTest.java
@@ -0,0 +1,31 @@
+package fightinggame.game;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class VectorTest {
+
+    @Test
+    @DisplayName("")
+    public void testAddVector() {
+
+    }
+
+    @Test
+    @DisplayName("")
+    public void testApplyAcceleration() {
+
+    }
+
+    @Test
+    @DisplayName("")
+    public void testGetVx() {
+
+    }
+
+    @Test
+    @DisplayName("")
+    public void testGetVy() {
+
+    }
+}
