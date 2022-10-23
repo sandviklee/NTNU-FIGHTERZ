44a073d3fa727ca3da1f2bd9c2811d8e08178c74
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-10-24 00:56:49 +0200

    Make VectorTest

--- gr2201/gameplay/src/test/java/fightinggame/game/VectorTest.java
+++ gr2201/gameplay/src/test/java/fightinggame/game/VectorTest.java
@@ -9,52 +9,111 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class VectorTest {
+    private int validVx;
+    private int validVy;
+    private int validAx;
+    private int validAy;
+    private int leftDirection;
+    private int rightDirection;
+
+    private int nonValidVx;
+    private int nonValidVy;
+    private int nonValidAx;
+    private int nonValidAy;
+
+    private Vector leftVector;
+    private Vector rightVector;
+    private Vector standardVec;
 
 
-     @Test
+    public void setup() {
+        validVx = 10;
+        validVy = 10;
+        validAx = 10;
+        validAy = 10;
+        leftDirection = -1;
+        rightDirection = 1;
+
+        leftVector = new Vector(validVx, validVy, validAx, validAy, leftDirection);
+        rightVector = new Vector(validVx, validVy, validAx, validAy, rightDirection);
+        standardVec = new Vector();
+    }
+    @Test
     @DisplayName("Check if the Constructor works ")        
     public void testConstructor() {
-        
         // clean test for Constructor
+        
         try {
-            new Vector();
-            new Vector();
+            leftVector = new Vector(validVx, validVy, validAx, validAy, leftDirection);
+            rightVector = new Vector(validVx, validVy, validAx, validAy, rightDirection);
+            standardVec = new Vector();
         }
         catch(Exception e) {
             fail();
             System.out.println(e.getLocalized);
         }
+        // check values for leftVector and rightVector
+        assertEquals(-validVx, leftVector.getVx() , "did not set correct cordinate");
+        assertEquals(-validVy, leftVector.getVy() , "did not set correct cordinate");
+        assertEquals(-validAx, leftVector.getAx() , "did not set correct cordinate");
+        assertEquals(-validAy, leftVector.getAy() , "did not set correct cordinate");
 
+        assertEquals(validVx, rightVector.getVx() , "did not set correct cordinate");
+        assertEquals(validVy, rightVector.getVy() , "did not set correct cordinate");
+        assertEquals(validAx, rightVector.getAx() , "did not set correct cordinate");
+        assertEquals(validAy, rightVector.getAy() , "did not set correct cordinate");
+        
+        // empty constructor
+        assertEquals(0, standardVec.getVx() , "did not set correct cordinate");
+        assertEquals(0, standardVec.getVy() , "did not set correct cordinate");
+        assertEquals(0, standardVec.getAx() , "did not set correct cordinate");
+        assertEquals(0, standardVec.getAy() , "did not set correct cordinate");
 
-        // Impropper constructions (dirty tests)
-        // bad height and length values
+        // dirty test for constructor
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            new EffectBox(validWorldEntity, validPoint, true, validLength, negNum);
+            new Vector(nonValidVx, nonValidVy, nonValidAx, nonValidAy, leftDirection);
+            new Vector(nonValidVx, nonValidVy, nonValidAx, nonValidAy, rightDirection);
+		}, "This Vector is not possible");
 
-		}, "This EffectBox is not possible, should not allow negativ values as height or length");
     }
     
     @Test
-    @DisplayName("")
+    @DisplayName("check that the vector adds the new vector to values")
     public void testAddVector() {
+        for (int i = 1; i < 100; i++) {
+            standardVec.addVector(rightVector);
 
+            assertEquals(i * validVx, standardVec.getVx() , "did not add correctly");
+            assertEquals(i * validVy, standardVec.getVy() , "did not add correctly");
+            assertEquals(i * validAx, standardVec.getAx() , "did not add correctly");
+            assertEquals(i * validAy, standardVec.getAy() , "did not add correctly");
+        }
+
+        standardVec = Vector();
+        for (int i = 1; i < 100; i++) {
+            standardVec.addVector(leftVector);
+            assertEquals(i * (-validVx), standardVec.getVx() , "did not add correctly");
+            assertEquals(i * (-validVy), standardVec.getVy() , "did not add correctly");
+            assertEquals(i * (-validAx), standardVec.getAx() , "did not add correctly");
+            assertEquals(i * (-validAy), standardVec.getAy() , "did not add correctly");
+        }
+        standardVec = Vector();
+        standardVec.addVector(leftVector);
+        standardVec.addVector(rightVector);
+
+        assertEquals(0, standardVec.getVx() , "did not add correctly");
+        assertEquals(0, standardVec.getVy() , "did not add correctly");
+        assertEquals(0, standardVec.getAx() , "did not add correctly");
+        assertEquals(0, standardVec.getAy() , "did not add correctly");
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("check if applyAcceleration adjusts velocity correctly")
     public void testApplyAcceleration() {
+        
 
-    }
+        // Try to apply zero acceleration
 
-    @Test
-    @DisplayName("")
-    public void testGetVx() {
-
-    }
-
-    @Test
-    @DisplayName("")
-    public void testGetVy() {
-
+        // 
     }
 }
