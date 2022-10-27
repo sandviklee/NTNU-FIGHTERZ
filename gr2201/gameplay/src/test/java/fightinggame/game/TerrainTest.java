5e3e5219c8a915900025a4974e983df34e2e76d0
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-10-27 00:30:30 +0000

    94.1 Remove unused imports

--- gr2201/gameplay/src/test/java/fightinggame/game/TerrainTest.java
+++ gr2201/gameplay/src/test/java/fightinggame/game/TerrainTest.java
@@ -3,10 +3,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
 
@@ -14,6 +12,7 @@ import org.junit.jupiter.api.Assertions;
 
 public class TerrainTest {
     private ArrayList<Integer> pos;
+    
     @BeforeEach
     public void setup() {
         pos = new ArrayList<>();
