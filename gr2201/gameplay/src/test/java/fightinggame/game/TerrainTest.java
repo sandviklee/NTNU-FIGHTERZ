4cf69e5c8add4818673085213d01f8f96de73567
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-10-27 00:01:48 +0000

    94.1 Make Terrain test

---
+++ gr2201/gameplay/src/test/java/fightinggame/game/TerrainTest.java
@@ -0,0 +1,33 @@
+package fightinggame.game;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Assertions;
+
+public class TerrainTest {
+    private ArrayList<Integer> pos;
+    @BeforeEach
+    public void setup() {
+        pos = new ArrayList<>();
+        pos.add(0);
+        pos.add(0);
+
+    }
+    @Test
+    @DisplayName("Check if constructor creates correct terrain object")
+    public void testConstructor() {
+        Terrain nonTraversableTerrain = new Terrain("terrain1", pos, false);
+        Terrain traversableTerrain = new Terrain("terrain2", pos, true);
+
+        assertTrue(traversableTerrain.isTraversable());
+        assertFalse(traversableTerrain.isTraversable());
+    }
+}
