553dddba7f85a2786c3a93646451e6eb4b6f5e9a
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-11-03 15:10:12 +0000

    82 add jackson to pom and module-info

--- gr2201/gameplay/src/main/java/module-info.java
+++ gr2201/gameplay/src/main/java/module-info.java
@@ -1,3 +1,6 @@
 module fightinggame.gameplay {
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
     exports fightinggame.game;
 }
