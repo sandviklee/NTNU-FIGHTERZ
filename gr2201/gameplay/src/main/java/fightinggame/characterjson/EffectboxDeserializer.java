c27e7abf285913f492160c119d740fa85ac1c67e
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-11-07 01:12:06 +0000

    82.6 Refactor add checks

--- gr2201/gameplay/src/main/java/fightinggame/characterjson/EffectboxDeserializer.java
+++ gr2201/gameplay/src/main/java/fightinggame/characterjson/EffectboxDeserializer.java
@@ -9,6 +9,8 @@ import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.BooleanNode;
+import com.fasterxml.jackson.databind.node.IntNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import fightinggame.game.Effectbox;
@@ -28,14 +30,18 @@ public class EffectboxDeserializer extends JsonDeserializer<Effectbox> {
             ObjectNode objectNode = (ObjectNode) jsonNode;
             JsonNode widthNode = objectNode.get("lenght");
             JsonNode heightNode = objectNode.get("height");
-
             JsonNode offsetXNode;
             JsonNode offsetYNode;
             JsonNode isTraversibleNode;
-            Boolean isHurtBox = true;
+
             Boolean isTraversible = false;
-            int width = widthNode.intValue();
-            int length = heightNode.intValue();
+            int width = 0;
+            int length = 0;
+            if ((widthNode instanceof IntNode) && (heightNode  instanceof IntNode)){
+                width = widthNode.intValue();
+                length = heightNode.intValue();
+            }
+
             int offsetX = 0;
             int offsetY = 0;
 
@@ -43,13 +49,14 @@ public class EffectboxDeserializer extends JsonDeserializer<Effectbox> {
                 offsetXNode = objectNode.get("offsetX");
                 offsetYNode = objectNode.get("offsetY");
                 isTraversibleNode = objectNode.get("isTraversible");
-                isHurtBox = false;
-                offsetX = offsetXNode.intValue();
-                offsetY = offsetYNode.intValue();
-                isTraversible = isTraversibleNode.booleanValue();
+                if ((offsetXNode instanceof IntNode) && (offsetYNode instanceof IntNode) && (isTraversibleNode instanceof BooleanNode)) {
+                    offsetX = offsetXNode.intValue();
+                    offsetY = offsetYNode.intValue();
+                    isTraversible = isTraversibleNode.booleanValue();
+                }
             }
                         
-            ArrayList hitboxProperties = new ArrayList<>();
+            ArrayList<Integer> hitboxProperties = new ArrayList<>();
             hitboxProperties.add(width);
             hitboxProperties.add(length);
 
