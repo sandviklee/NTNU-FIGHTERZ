db5b188f50c67bfaba5ca1a1b23abec1469b4c7c
Kristoffer Olaisen <krisnol@stud.ntnu.no>
2022-11-10 15:51:06 +0100

    117.1: finished method getUser

--- gr2201/fxui/src/main/java/fightinggame/ui/RemoteModelAccess.java
+++ gr2201/fxui/src/main/java/fightinggame/ui/RemoteModelAccess.java
@@ -6,20 +6,34 @@ import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
 
 import fightinggame.users.User;
-
-import java.net.http.HttpResponse;
 
 public class RemoteModelAccess {
 
     private URI uri;
+    private User user;
 
     public RemoteModelAccess (URI uri){
         this.uri = uri;
     }
     
     public User getUser(String username, String password){
-        
+        HttpRequest request = HttpRequest.newBuilder(uri).headers("username", username, "password", password ).GET().build();
+        try {
+            final HttpResponse<String> response = HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+            this.user = response.body();
+          }
+           catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+          }
+        return this.user;
     }
+
+    public boolean putUser(String username, String password, String confirmPassword){
+        HttpRequest request = HttpRequest.newBuilder(uri).headers()
+    }
+
+
 }
