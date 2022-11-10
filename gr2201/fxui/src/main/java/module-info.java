1657606310bc85dcc45830eff2c0597264fe455e
Sverre Nystad <sverrnys@ntnu.no>
2022-11-10 23:47:39 +0100

    155.1 add jackson to module-info

--- gr2201/fxui/src/main/java/module-info.java
+++ gr2201/fxui/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module fightinggame.ui {
     requires transitive javafx.graphics;
     requires fightinggame.base;
     requires transitive fightinggame.gameplay;
+    requires com.fasterxml.jackson.databind;
     
     opens fightinggame.ui to javafx.fxml, javafx.graphics;
     exports fightinggame.ui;
