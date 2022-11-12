ffbd28e29f440e1008a7d3b4156d9e8bebd04300
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-11-12 17:50:18 +0000

    118.3 Change visibility of dbaccess so restserver can access DAO

--- gr2201/base/src/main/java/module-info.java
+++ gr2201/base/src/main/java/module-info.java
@@ -3,5 +3,6 @@ module fightinggame.base {
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.annotation;
     exports fightinggame.users;
+    exports fightinggame.dbaccess;
     // Do not want dbaccess to be open
 }
