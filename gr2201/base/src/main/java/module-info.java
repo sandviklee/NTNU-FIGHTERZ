fc74f9b16adde3751fc6d4c45fc2b2d92dfff5e2
Kristoffer Nohr Olaisen <krisnol@stud.ntnu.no>
2022-11-12 16:32:31 +0000

    117.2: Finish method

--- gr2201/base/src/main/java/module-info.java
+++ gr2201/base/src/main/java/module-info.java
@@ -3,5 +3,6 @@ module fightinggame.base {
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.annotation;
     exports fightinggame.users;
+    exports fightinggame.json;
     // Do not want dbaccess to be open
 }
--- gr2201/fxui/src/main/java/fightinggame/ui/RemoteModelAccess.java
+++ gr2201/fxui/src/main/java/fightinggame/ui/RemoteModelAccess.java
@@ -1,6 +1,9 @@
 package fightinggame.ui;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
+
 import java.net.URI;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
@@ -9,9 +12,8 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.nio.charset.StandardCharsets;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import fightinggame.json.*;
+import fightinggame.json.UserModule;
 import fightinggame.users.User;
 
 public class RemoteModelAccess {
@@ -24,13 +26,14 @@ public class RemoteModelAccess {
     public RemoteModelAccess (URI uriBase){
         this.uriBase = uriBase;
         this.mapper = new ObjectMapper();
+        mapper.registerModule(new UserModule());
         
     }
 
     private String uriParam(String param) {
         return URLEncoder.encode(param, StandardCharsets.UTF_8);
       }
-    
+    // headers to http request might be added
     public User getUser(String username, String password){
         HttpRequest request = HttpRequest.newBuilder(uriBase.resolve(uriParam(username) + "/").resolve(uriParam(password))).GET().build();
         try {
@@ -44,8 +47,18 @@ public class RemoteModelAccess {
         return this.user;
     }
 
-    public boolean putUser(String username, String password, String confirmPassword){
-        HttpRequest request = HttpRequest.newBuilder(uri).headers("username", username, "password", password, "confirmPassword", confirmPassword).GET().build();
+    public User putUser(String username, String password, String confirmPassword){
+        HttpRequest request = HttpRequest.newBuilder(uriBase.resolve(uriParam(username)))
+        .PUT(BodyPublishers.ofString(username + "." + password + "." + confirmPassword)).build();
+        try {
+            HttpResponse<String> response = HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+            String userString = response.body();
+            this.user = mapper.readValue(userString, User.class);
+
+        } catch (IOException| InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return this.user;
     }
 
 
