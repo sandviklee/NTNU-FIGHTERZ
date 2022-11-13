1e2abc70d3599305036ca086fa672289b1b6de7b
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-11-13 02:44:54 +0000

    118.5 Remove unused import

--- gr2201/springboot/restserver/src/main/java/fightinggame/springboot/restserver/ServerApplication.java
+++ gr2201/springboot/restserver/src/main/java/fightinggame/springboot/restserver/ServerApplication.java
@@ -2,7 +2,6 @@ package fightinggame.springboot.restserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 /**
