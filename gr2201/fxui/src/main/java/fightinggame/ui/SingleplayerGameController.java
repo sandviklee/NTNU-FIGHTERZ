9566d8c96c6576b6fe844db57d0364f2c3b58b7b
SaiYueWasTaken <74411800+SaiYueWasTaken@users.noreply.github.com>
2022-11-14 00:44:14 +0100

    155 Fine tuning and fix rendering

--- gr2201/fxui/src/main/java/fightinggame/ui/SingleplayerGameController.java
+++ gr2201/fxui/src/main/java/fightinggame/ui/SingleplayerGameController.java
@@ -25,20 +25,12 @@ public class SingleplayerGameController extends SceneController{
     private HashMap<String, Image> playerSprites = new HashMap<>();
     private boolean paused;
      
-    private ArrayList<String> player1Keys = new ArrayList<>(Arrays.asList(".", ",", "W", "D", "A", "DW", "AW", "B", "DB", "AB", "WB", "SB", "DV", "AV", "WV", "SV"));
+    private ArrayList<String> player1Keys = new ArrayList<>(Arrays.asList(".", ",", "W", "D", "A", "DW", "AW", "V", "DV", "AV", "WV", "SV", "DC", "AC", "WC", "SC", "C", "S"));
+    private ArrayList<String> player2Keys = new ArrayList<>(Arrays.asList(".", ",", "I", "L", "J", "LI", "JI", "N", "LN", "JN", "IN", "KN", "LM", "JM", "IM", "KM", "M", "K"));
 
-    // Should switch to Point
     private ArrayList<Integer> playerPosition = new ArrayList<>(Arrays.asList(500, 500));
     private ArrayList<Integer> terrainPosition = new ArrayList<>(Arrays.asList(800, 910));
-
     private ArrayList<Integer> dummyPosition = new ArrayList<>(Arrays.asList(1100, 500));
-
-    // private Point playerPosition = new Point(925, 730);
-    // private Point terrainPosition = new Point(1330, 950);
-    // private Point terrain2Position = new Point(1200, 550);
-    // private Point terrain3Position = new Point(500, 950);
-    // private Point dummyPosition = new Point(400, 400);
-
     
     private SpriteRenderer renderer;
     private String keyInputs = "";
@@ -48,7 +40,8 @@ public class SingleplayerGameController extends SceneController{
     public void loadWorld(String character, String gameStage){
         worldCanvas.setFocusTraversable(true);
         GameCharacter player = loadPlayer(character, playerPosition, player1Keys);
-        GameCharacter dummy = loadPlayer("Dummy", dummyPosition);
+        GameCharacter player2 = loadPlayer(character, dummyPosition, player2Keys);
+        //GameCharacter dummy = loadPlayer("Dummy", dummyPosition);
         Terrain terrain = loadTerrain("Test", terrainPosition, 1300, 200);
         loadCharacterSprite(character, playerSprites);
         loadCharacterSprite("Dummy", playerSprites);
@@ -56,7 +49,9 @@ public class SingleplayerGameController extends SceneController{
         
         //System.out.println(playerSprites);
         worldEntities.add(player);
-        worldEntities.add(dummy);
+
+        worldEntities.add(player2);
+        //worldEntities.add(dummy);
         worldEntities.add(terrain);
 
         world = new World(worldEntities);
@@ -89,14 +84,13 @@ public class SingleplayerGameController extends SceneController{
     }
 
     private void updateWorld() {
-        
         new AnimationTimer() {
             private long tick = 0;
             @Override
             public void handle(long now) {
                 if (now - tick >= fps) {
                     if (world.getWorldEntities().stream().anyMatch(x -> !worldEntities.contains(x))) {
-                        worldEntities = world.getWorldEntities();
+                        renderer.updateRendererWorldEntities(world.getWorldEntities());
                     }
                     world.updateWorld(keyInputs, keyReleased);
                     renderer.update();
@@ -119,8 +113,6 @@ public class SingleplayerGameController extends SceneController{
         this.keyInputs = "";
         this.keyReleased = "";
     }
-
-    private 
 
     @FXML
     private void handleKeyPressed(KeyEvent event){
