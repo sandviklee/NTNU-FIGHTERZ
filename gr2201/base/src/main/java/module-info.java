ff00d8e1cb7fbef8f234d8b221f8850b0f42fb84
Kristoffer Olaisen <krisnol@stud.ntnu.no>
2022-11-14 16:52:17 +0100

    193: Make fightinggame.base export json

--- gr2201/base/src/main/java/module-info.java
+++ gr2201/base/src/main/java/module-info.java
@@ -4,5 +4,6 @@ module fightinggame.base {
     requires com.fasterxml.jackson.annotation;
     exports fightinggame.users;
     exports fightinggame.dbaccess;
+    exports fightinggame.json;
     // Do not want dbaccess to be open
 }
