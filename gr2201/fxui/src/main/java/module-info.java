c149ff517e96670a54eab0dec04d6cc1271d4212
SaiYue <74411800+SaiYueWasTaken@users.noreply.github.com>
2022-11-15 14:13:55 +0100

    155 Fix module-info typo

--- gr2201/fxui/src/main/java/module-info.java
+++ gr2201/fxui/src/main/java/module-info.java
@@ -7,7 +7,6 @@ module fightinggame.ui {
     requires transitive com.fasterxml.jackson.annotation;
     requires fightinggame.base;
     requires transitive fightinggame.gameplay;
-    requires com.fasterxml.jackson.databind;
     
     opens fightinggame.ui to javafx.fxml, javafx.graphics;
     exports fightinggame.ui;
