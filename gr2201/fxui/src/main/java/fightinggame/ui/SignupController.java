361f2667493bc015cb6439516185e493eee5bb64
Elin <elihaugu@stud.ntnu.no>
2022-11-16 18:23:00 +0100

    130 Add css for tutorial (needs to be fixed, temporary)

--- gr2201/fxui/src/main/java/fightinggame/ui/SignupController.java
+++ gr2201/fxui/src/main/java/fightinggame/ui/SignupController.java
@@ -18,6 +18,7 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.Pane;
@@ -50,38 +51,67 @@ public class SignupController extends SceneController{
             return;
         }
 
-        Alert alert = new Alert(AlertType.INFORMATION);
-        alert.setTitle("Welcome to NTNU Fighterz!");
-        alert.setHeaderText("Welcome to NTNU Fighterz!");
-
-        File tutorialText = new File(getClass().getResource("Tutorial.txt").toExternalForm());
-        Scanner reader = new Scanner(tutorialText);
-        String tutorial = "";
-        while (reader.hasNextLine()){
-            tutorial = tutorial + reader.nextLine();
-        }
-        reader.close();
-
-        Label label = new Label(tutorial);
-        label.setWrapText(true);
-        Font font = new Font("Comic Sans MS", 30);
-        label.setFont(font);
-        alert.getDialogPane().setContent(label);
-
-        DialogPane dialogPane = alert.getDialogPane();
-        String cssPath = getClass().getResource("css/popup.css").toString();
-        dialogPane.getStylesheets().add(cssPath);
-        dialogPane.getStyleClass().add("popup");
-        dialogPane.setPrefSize(1080, 640);;
-        ((Button) dialogPane.lookupButton(ButtonType.OK)).setFont(font);
-
-        alert.showAndWait();
+        showTutorial();
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
         Parent root = loader.load();
         MainMenuController mainMenuController = loader.getController();
         mainMenuController.setUser(tempUser);
         super.changeScene("NTNU Fighterz", root, event);
+    }
+
+    private void showTutorial() {
+        // create alert
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Welcome to NTNU Fighterz!");
+        alert.setHeaderText("Welcome to NTNU Fighterz!");
+        
+        // get tutorial
+        String tutorial = "";
+        try {
+            File tutorialText = new File("../fxui/src/main/resources/fightinggame/ui/Tutorial.txt");
+            tutorial = readFromFile(tutorialText);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        String cssPath = getClass().getResource("css/popup.css").toString();
+
+        // set and style content text
+        TextArea area = new TextArea(tutorial);
+        area.setId("TutorialContent");
+        alert.getDialogPane().setContent(area);
+        TextArea content = (TextArea) alert.getDialogPane().lookup("TutorialContent");
+
+        area.setWrapText(true);
+        area.setEditable(false);
+        area.getStylesheets().add(cssPath);
+        area.getStyleClass().add("textarea");
+
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.getStylesheets().add(cssPath);
+        dialogPane.getStyleClass().add("popup");
+        dialogPane.setPrefSize(1280, 720);;
+
+        alert.showAndWait();
+    }
+    
+    //TODO: fix persistence thing
+    private static String readFromFile(File userFile) throws IOException {		
+		String usersInfo = "";
+        System.out.println(userFile);
+		if (userFile.exists()){
+            System.out.println("Yes");
+			Scanner userFileReader = new Scanner(userFile);
+
+			while(userFileReader.hasNextLine()) {
+				String line = userFileReader.nextLine();
+				usersInfo += line + "\n";
+			}
+			userFileReader.close();
+
+		}
+		return usersInfo;
     }
 
     @FXML 
