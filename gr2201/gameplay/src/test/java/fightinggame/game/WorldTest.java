4f1901db816571ff4e8ef53e14ead4c89cde81e1
Kristoffer Olaisen <krisnol@stud.ntnu.no>
2022-11-17 19:20:54 +0100

    216.4 Add test

--- gr2201/gameplay/src/test/java/fightinggame/game/WorldTest.java
+++ gr2201/gameplay/src/test/java/fightinggame/game/WorldTest.java
@@ -8,27 +8,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class WorldTest {
     
 
     @Test
-    @DisplayName("")
+    @DisplayName("Tests for World constructor")
     public void testConstructor() {
+        Terrain terrain = new Terrain("test", new ArrayList<>(Arrays.asList(0, 0)), 10, 10);
 
-        // must
-    }
+        ArrayList<WorldEntity> worldEntities = new ArrayList<>();
+        worldEntities.add(terrain);
 
-    @Test
-    @DisplayName("")
-    public void testAddWorldEntity() {
+        World world = new World(worldEntities);
 
-        // try to add null
-    }
-
-    @Test
-    @DisplayName("")
-    public void testUpdateWorld() {
-
-        // try to add null
+        assertEquals("test", world.getWorldEntities().get(0).getName());
+        
     }
 }
