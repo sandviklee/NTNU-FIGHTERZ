374610700dac1a86f8e68022c9ce443090f2ba09
Sverre Nystad <sverrnys@stud.ntnu.no>
2022-11-22 20:32:26 +0100

    166.9 correct modules and pom

--- gr2201/base/src/main/java/module-info.java
+++ gr2201/base/src/main/java/module-info.java
@@ -1,9 +1,14 @@
 module fightinggame.base {
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
-    requires com.fasterxml.jackson.annotation;
+    requires transitive com.fasterxml.jackson.core;
+    requires transitive com.fasterxml.jackson.databind;
+    requires transitive com.fasterxml.jackson.annotation;
+    requires transitive fightinggame.gameplay;
     exports fightinggame.users;
-    exports fightinggame.dbaccess;
-    exports fightinggame.json;
+
+    exports fightinggame.dao; // to fightinggame.ui, fightinggame.springboot.restserver;
+    exports fightinggame.utils; // to fightinggame.springboot.restserver, fightinggame.ui;
+    exports fightinggame.utils.json.characterattributes; // to fightinggame.ui;
+    exports fightinggame.utils.json.characterinfo; // to fightinggame.ui;
+    exports fightinggame.utils.json.users; // to fightinggame.ui;
     // Do not want dbaccess to be open
 }
